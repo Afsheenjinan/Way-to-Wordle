397a3f9fc8865501470cf7dbd6c18947cdb21464
Afsheenjinan <afsheenameen@gmail.com>
2022-06-16 14:32:01 +0530

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wordle_clone/board_layout.dart';
 
 import 'alphabet.dart';
@@ -30,39 +31,38 @@ class _HomePageState extends State<HomePage> {
   void _onEnter() {
     if (_currentword.contains(' ')) return;
 
-    if (rowIndex < 5) {
-      List<Alphabet> alphabetList = _currentword.toAlphabetList();
+    if (rowIndex > 5) return;
 
-      for (var i = 0; i < _reference.length; i++) {
-        print('$_currentword $_reference');
-        if (_currentword[i] == _reference[i]) {
-          alphabetList[i].bgColor = Colors.green;
-          bag.add(alphabetList[i]);
-        } else if (_reference.contains(_currentword[i])) {
-          alphabetList[i].bgColor = Colors.yellow;
-          bag.add(alphabetList[i]);
-        } else {
-          alphabetList[i].bgColor = Colors.grey;
-        }
+    List<Alphabet> alphabetList = _currentword.toAlphabetList();
 
-        final Alphabet tileInBag = bag.firstWhere((e) => e.letter == _currentword[i], orElse: () => Alphabet(letter: ' '));
-
-        if (tileInBag.bgColor != Colors.green) {
-          bag.removeWhere((e) => tileInBag.letter == e.letter);
-          bag.add(alphabetList[i]);
-        }
+    for (var i = 0; i < _reference.length; i++) {
+      if (_currentword[i] == _reference[i]) {
+        alphabetList[i].bgColor = Colors.green;
+        bag.add(alphabetList[i]);
+      } else if (_reference.contains(_currentword[i])) {
+        alphabetList[i].bgColor = Colors.yellow;
+        bag.add(alphabetList[i]);
+      } else {
+        alphabetList[i].bgColor = Colors.grey;
       }
-      setState(() {
-        _list[rowIndex] = alphabetList;
-        if (_currentword != _reference) {
-          _currentword = '     ';
-          _list.add(_currentword.toAlphabetList());
-        }
-      });
 
-      rowIndex += 1;
-      columnIndex = 0;
+      final Alphabet tileInBag = bag.firstWhere((e) => e.letter == _currentword[i], orElse: () => Alphabet(letter: ' '));
+
+      if (tileInBag.bgColor != Colors.green) {
+        bag.removeWhere((e) => tileInBag.letter == e.letter);
+        bag.add(alphabetList[i]);
+      }
     }
+    setState(() {
+      _list[rowIndex] = alphabetList;
+      if (_currentword != _reference) {
+        _currentword = '     ';
+        if (rowIndex < 5) _list.add(_currentword.toAlphabetList());
+      } else {}
+    });
+
+    rowIndex += 1;
+    columnIndex = 0;
   }
 
   void _onBackspace() {
@@ -77,6 +77,7 @@ class _HomePageState extends State<HomePage> {
 
   void _onKey(String val) {
     if (columnIndex >= _currentword.length) return;
+    if (rowIndex > 5) return;
 
     setState(() {
       _currentword = _currentword.replacewith(columnIndex, val);
@@ -87,24 +88,45 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('WORDLE in a Minute'),
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Spacer(),
-            Board(keys: _list),
-            const Spacer(),
-            Keyboard(onEnter: _onEnter, onDelete: _onBackspace, onKey: _onKey, scrabbleTiles: bag),
-          ],
+    return KeyboardListener(
+      focusNode: FocusNode(),
+      autofocus: true,
+      onKeyEvent: _onKeyEvent,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('WORDLE in a Minute'),
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Spacer(),
+              Board(keys: _list),
+              const Spacer(),
+              Keyboard(onEnter: _onEnter, onDelete: _onBackspace, onKey: _onKey, scrabbleTiles: bag),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  KeyEventResult _onKeyEvent(event) {
+    if (event is KeyUpEvent) {
+      if (event.logicalKey == LogicalKeyboardKey.enter) {
+        _onEnter;
+        return KeyEventResult.handled;
+      } else if (event.logicalKey == LogicalKeyboardKey.backspace) {
+        _onBackspace;
+        return KeyEventResult.handled;
+      } else {
+        _onKey(event.logicalKey.keyLabel);
+        return KeyEventResult.handled;
+      }
+    }
+    return KeyEventResult.ignored;
   }
 }
